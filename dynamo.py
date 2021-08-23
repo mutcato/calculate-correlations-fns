8735f5bdf1b46302031fca9d1708357894efae49
Mutlucan Tokat <6348274-mutlucan@users.noreply.gitlab.com>
2021-08-24 02:23:18 +0300

    Inherit Item and Summary tables from Table class

--- dynamo.py
+++ dynamo.py
@@ -1,68 +1,76 @@
 import boto3
+import pandas as pd
 
-class DataFrame:
-    def __init__(self, table_name:str):
-        resource = boto3.resource(
+
+class Table:
+    def __init__(self, table_name):
+        self.resource = boto3.resource(
             "dynamodb"
         )
-        self.table = resource.Table(table_name)
-        
-    def get_item(self, partition_key, sort_key):
-        response = self.table.get_item(Key={
-            partition_key["name"]: partition_key["value"],
-            sort_key["name"]: sort_key["value"]
-        })
-        return response
+        self.table_name = table_name
 
+        self.table = self.resource.Table(self.table_name)
+
+class Item(Table):
+    def __init__(self, partition_key:dict, sort_key:dict):
+        Table.__init__(self, "metrics2")
+        self.partition_key = partition_key
+        self.sort_key = sort_key
+        self.data = self.get()
+
+    def get(self):
+        response = self.table.get_item(Key={
+            self.partition_key["name"]: self.partition_key["value"],
+            self.sort_key["name"]: self.sort_key["value"]
+        })
+        return {response["Item"][self.partition_key["name"]]:  response["Item"]["metric_values"]}
+
+    def prepare_for_dataframe(self):
+        ticker = list(self.data.keys())[0]
+        close_prices = self.data[ticker]
+        timestamps, closes = [], []
+        for time_close_pair in close_prices:
+            timestamp = int(list(time_close_pair.keys())[0])
+            timestamps.append(timestamp)
+            close_price = list(time_close_pair.values())[0]
+            closes.append(close_price)
+            
+        return {"ticker": ticker, "closes": closes, "timestamps": timestamps}
+
+    def convert_to_dataframe(self):
+        prepared_item = self.prepare_for_dataframe()
+        d = {
+            prepared_item["ticker"]: pd.Series(prepared_item["closes"], index=prepared_item["timestamps"])
+        }
+        return pd.DataFrame(d)
+
+class DataFrameTable:
+    def __init__(self):
+        pass
 
     @staticmethod
-    def get_longest_shortest_items(first_item, second_item):
-        """
-        Returns a tupple first element of the tupple is longest second element of the tupple is shortest.
-        (longest_item, shortest_item)
-        """
-        length_of_the_longest = max(len(first_item["timestamps"]), len(second_item["timestamps"]))
-        if len(first_item["timestamps"]) == length_of_the_longest:
-            return first_item, second_item
-        elif len(second_item["timestamps"]) == length_of_the_longest:
-            return second_item, first_item
+    def get_all_items(interval, metric):
+        summary = Summary()
+        filtered_tickers = summary.filter_tickers(interval, metric)
+        items_as_df = []
+        for index, ticker in enumerate(filtered_tickers):
+            partition_key = {"name": "ticker", "value": ticker}
+            sort_key = {"name": "interval_metric", "value": f"{interval}_{metric}"}
+            item = Item(partition_key, sort_key)
+            items_as_df.append(item.convert_to_dataframe())
+            if index == 6:
+                break
 
-    def equalize(self, ticker1:str, ticker2:str, interval_metric:str):
-        """
-        Looks timestamps arrays in two array. Removes timestamps which are not exist in both two items and removes corresponding metric_value
-        """
-        first_item = [item for item in self.all_items if (item ["ticker"] == ticker1 and item["interval_metric"] == interval_metric)][0]
-        second_item = [item for item in self.all_items if (item ["ticker"] == ticker2 and item["interval_metric"] == interval_metric)][0]
-        if len(first_item["timestamps"]) == len(second_item["timestamps"]):
-            return first_item, second_item
+        return items_as_df
 
-        difference = list(set(first_item["timestamps"])^set(second_item["timestamps"]))
-
-        for timestamp in difference:
-            if timestamp in first_item["timestamps"]:
-                del first_item["metric_values"][first_item["timestamps"].index(timestamp)]
-                first_item["timestamps"].remove(timestamp)
-
-            if timestamp in second_item["timestamps"]:
-                del second_item["metric_values"][second_item["timestamps"].index(timestamp)]
-                second_item["timestamps"].remove(timestamp)
-
-        return first_item, second_item
-
-    def build_dataframe(self, tickers, interval_metric):
-        for ticker in tickers:
-            self.get_item(ticker, interval_metric)
-            import pdb
-            pdb.set_trace()
+    def build(self, interval, metric):
+        dataframes = self.get_all_items(interval, metric)
+        return pd.concat(dataframes, axis=1)
 
 
-class Summary:
+class Summary(Table):
     def __init__(self):
-        resource = boto3.resource(
-            "dynamodb"
-        )
-        table_name = "metrics_summary"
-        self.table = resource.Table(table_name)
+        Table.__init__(self, "metrics_summary")
         self.all_items = self.get_all_items()
 
     def get_all_items(self):
